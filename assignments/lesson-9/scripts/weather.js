f79cd811259552d7a96163a55d99cfa705f37d64
j2therod <38600082+j2therod@users.noreply.github.com>
2018-06-28 09:41:59 -0500

    Add files via upload

--- assignments/lesson-9/scripts/weather.js
+++ assignments/lesson-9/scripts/weather.js
@@ -1,10 +1,20 @@
 var weatherObject = new XMLHttpRequest();
-weatherObject.open('Get','http://api.openweathermap.org/data/2.5/weather?zip=55333,usappid=d246864f4acf9a85cbafb96d6e8c32da',true);
+weatherObject.open('GET', 'http://api.openweathermap.org/data/2.5/weather?zip=55333,us&appid=d246864f4acf9a85cbafb96d6e8c32da', true);
 
-weatherOjbect.send();
+weatherObject.send();
 
 weatherObject.onload = function() {
-    var weatherInfo = JSON.parse(weather.ObjectresponseText);
-    cosole.log(weatherInfo);
-    document.getElementById('plsce').innerHTML = weatherInfo.name;
+    var weatherInfo = JSON.parse(weatherObject.responseText);
+    console.log(weatherInfo);
+    
+    document.getElementById('place').innerHTML = weatherInfo.name;
+    document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp;
+    document.getElementById('wind').innerHTML = weatherInfo.wind.speed;
+    
+    var iconcode = weatherInfo.weather[0].icon;
+    
+    var icon_path = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    document.getElementById('weather_icon').src = icon_path;
 }
+
+//end of function
